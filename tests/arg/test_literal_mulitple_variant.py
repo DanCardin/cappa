b7e8cc3dad2f3dae45ef61f7f6c75969f613ac85
DanCardin <ddcardin@gmail.com>
2023-10-19 08:53:30 -0400

    fix: Literal["one", "two"] syntax as choices.

---
+++ tests/arg/test_literal_mulitple_variant.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Literal
+
+import cappa
+import pytest
+
+from tests.utils import backends, parse
+
+
+@dataclass
+class ArgTest:
+    name: Literal["one", "two", "three", 4]
+
+
+@backends
+def test_literal(backend):
+    test = parse(ArgTest, "two", backend=backend)
+    assert test.name == "two"
+
+    test = parse(ArgTest, "4", backend=backend)
+    assert test.name == 4
+
+    with pytest.raises(cappa.Exit) as e:
+        parse(ArgTest, "thename", backend=backend)
+
+    message = str(e.value.message).lower()
+    assert (
+        "invalid choice: 'thename' (choose from 'one', 'two', 'three', '4')" in message
+    )
