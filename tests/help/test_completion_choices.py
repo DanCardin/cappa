0b8917a35af9824b102e6ba3d0d7f3494603d399
DanCardin <ddcardin@gmail.com>
2023-10-19 07:34:16 -0400

    Add test for non-duplicated choice help text.

---
+++ tests/help/test_completion_choices.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+import re
+from dataclasses import dataclass
+
+import cappa
+import pytest
+
+from tests.utils import parse
+
+
+def test_string_group(capsys):
+    @dataclass
+    class Args:
+        ...
+
+    with pytest.raises(cappa.HelpExit) as e:
+        parse(Args, "--help", backend=cappa.backend)
+
+    assert e.value.code == 0
+
+    out = capsys.readouterr().out
+
+    options = re.findall(r"Valid\s+options:\s+generate,\s+complete", out, re.MULTILINE)
+    assert len(options) == 1
