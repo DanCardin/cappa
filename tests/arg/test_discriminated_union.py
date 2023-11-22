7bb5b1f7828c5df632232e8667cec7a5acdfd879
DanCardin <ddcardin@gmail.com>
2023-11-22 08:56:10 -0500

    feat: Support "discriminated union".
    i.e. unions which have type distinctions that affect how they're mapped.

---
+++ tests/arg/test_discriminated_union.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Literal, Tuple, Union
+
+import pytest
+
+from tests.utils import backends, parse
+
+
+@backends
+def test_valid_tagged_unions(backend):
+    @dataclass
+    class ArgTest:
+        name: Union[
+            Tuple[Literal["one"], str],
+            Tuple[Literal["two"], int],
+            Tuple[
+                Literal["three"],
+                float,
+            ],
+        ]
+
+    test = parse(ArgTest, "one", "string", backend=backend)
+    assert test.name == ("one", "string")
+
+    test = parse(ArgTest, "two", "4", backend=backend)
+    assert test.name == ("two", 4)
+
+    test = parse(ArgTest, "three", "1.4", backend=backend)
+    assert test.name == ("three", 1.4)
+
+
+@backends
+def test_disallowed_different_arity_variants(backend):
+    @dataclass
+    class ArgTest:
+        name: Union[Tuple[str, str], Tuple[str, str, str]]
+
+    with pytest.raises(ValueError) as e:
+        parse(ArgTest, "one", "string", backend=backend)
+
+    assert (
+        str(e.value).lower().replace("typing.", "")
+        == "on field 'name', mismatch of arity between union variants. `tuple[str, str]` produces `num_args=2`, `tuple[str, str, str]` produces `num_args=3`."
+    )
