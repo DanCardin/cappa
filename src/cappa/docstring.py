54e52ea77c2e923e0e7546c383f60800cf5b9cc0
DanCardin <ddcardin@gmail.com>
2024-10-17 21:57:21 -0400

    feat: Add new "method" style of defining subcommands.

--- src/cappa/docstring.py
+++ src/cappa/docstring.py
@@ -79,10 +79,15 @@ def get_attribute_docstrings(command: type) -> dict[str, str]:
         return result
 
     source = textwrap.dedent(raw_source)
-    module = ast.parse(source)
+
+    try:
+        module = ast.parse(source)
+    except Exception:
+        return result
 
     cls_node = module.body[-1]
-    assert isinstance(cls_node, ast.ClassDef), cls_node
+    if not isinstance(cls_node, ast.ClassDef):
+        return result
 
     last_assignment: ast.AnnAssign | None = None
     for node in cls_node.body:
